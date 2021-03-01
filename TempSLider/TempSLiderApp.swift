8dab5f0fef03a5ef140ff040bf607a8b9ca0e466
adamjohnlea <imnogeekdptcom@me.com>
2021-02-28 17:29:55 -0700

    Initial Commit

---
+++ TempSLider/TempSLiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempSLiderApp.swift
+//  TempSLider
+//
+//  Created by Adam Lea on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TempSLiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
